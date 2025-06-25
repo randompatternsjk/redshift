7f07e7ca94a43d6a426ef641f4a617007e9970f8
randompatternsjk <me@jordankeller.work>
2025-06-25 14:36:32 -0500

    Define lyrics collection in content.config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "static", // Generate static files at build time
+
+  // Generate static files at build time
   // markdown: {
   //   site: "https://www.theokaylakes.com", // Use your custom domain if it's properly configured
   // },
+  output: "static",
+
+  adapter: vercel(),
 });
